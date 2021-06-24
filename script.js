d88dc741435f123e47607c9603b85e7267d4b129
stefandrei <stefandrei89@outlook.com>
2021-06-24 13:41:02 +0200

    add/Ex2

--- script.js
+++ script.js
@@ -1,16 +1,25 @@
+// // function crystalGazer( job_title, location, partners_name, number_childrens) {
+// //   text_ = `You will be a ${job_title} in ${location} and married to ${partners_name} with ${number_childrens}`;
+// //   return text_
+// // }
+
+// // document.write(crystalGazer("waiter", "Rome", "Aurora", 1));
+// // console.log(crystalGazer("wtw", "here", "her", 0));
+
+
 // function crystalGazer( job_title, location, partners_name, number_childrens) {
-//   text_ = `You will be a ${job_title} in ${location} and married to ${partners_name} with ${number_childrens}`;
-//   return text_
-// }
+//     document.write(`You will be a ${job_title} in ${location} and married to ${partners_name} with ${number_childrens}`);
+//     return 
+//   }
 
-// document.write(crystalGazer("waiter", "Rome", "Aurora", 1));
-// console.log(crystalGazer("wtw", "here", "her", 0));
+//   crystalGazer("artist", "Florence", "none", 3);
 
-
-function crystalGazer( job_title, location, partners_name, number_childrens) {
-    document.write(`You will be a ${job_title} in ${location} and married to ${partners_name} with ${number_childrens}`);
-    return 
-  }
-
-  crystalGazer("artist", "Florence", "none", 3);
+//Age calculator//
  
+
+function ageCalculator(birth_year, current_year)
+{var myAge = current_year - birth_year;
+var yourAge = myAge - 1;
+return yourAge;}
+document.write(ageCalculator(1989, 2022));
+
