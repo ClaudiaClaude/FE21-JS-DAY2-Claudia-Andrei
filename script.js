7925514c3b3c60b890da8fd8c382cfe448bdb66c
Claudia Tomassetti <hagol.cker@gmail.com>
2021-06-24 14:18:01 +0200

    finishes basic ex. 2 and 3

--- script.js
+++ script.js
@@ -11,15 +11,46 @@
 //     document.write(`You will be a ${job_title} in ${location} and married to ${partners_name} with ${number_childrens}`);
 //     return 
 //   }
-
 //   crystalGazer("artist", "Florence", "none", 3);
 
-//Age calculator//
+//Age calculator// ex 2 and 3
  
 
-function ageCalculator(birth_year, current_year)
-{var myAge = current_year - birth_year;
-var yourAge = myAge - 1;
-return yourAge;}
-document.write(ageCalculator(1989, 2022));
+function ageCalculator(birth_year, current_year){
+    
+    var myAge = current_year - birth_year;
+    var yourAge = myAge - 1;
+    return `You are either ${myAge} or ${yourAge}`;
+
+}
+
+document.write(ageCalculator(1989, 2021));
+
+
+function ageCalculator2 (birthYear) {
+
+    var currYear = new Date().getFullYear();
+    var myAge = currYear - birthYear;
+    var yourAge = myAge - 1;
+    return `You are either ${myAge} or ${yourAge}`;
+
+}
+document.write("<br>")
+document.write(ageCalculator2(1989));
+
+// document.write(new Date().getFullYear());
+
+// document.write(getFullYear());
+
+function converter (degrees) {
+
+    return degrees * (Math.PI / 180);
+}
+
+document.write("<br>");
+document.write(converter(90));
+// document.write("<br>");
+// document.write(Math.PI);
+
+
 
