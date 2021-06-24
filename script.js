598e3586ca226d3a5bac98e1de90fec0119e25bc
Claudia Tomassetti <hagol.cker@gmail.com>
2021-06-24 11:44:44 +0200

    adds basic ex 1

---
+++ script.js
@@ -0,0 +1,16 @@
+// function crystalGazer( job_title, location, partners_name, number_childrens) {
+//   text_ = `You will be a ${job_title} in ${location} and married to ${partners_name} with ${number_childrens}`;
+//   return text_
+// }
+
+// document.write(crystalGazer("waiter", "Rome", "Aurora", 1));
+// console.log(crystalGazer("wtw", "here", "her", 0));
+
+
+function crystalGazer( job_title, location, partners_name, number_childrens) {
+    document.write(`You will be a ${job_title} in ${location} and married to ${partners_name} with ${number_childrens}`);
+    return 
+  }
+
+  crystalGazer("artist", "Florence", "none", 3);
+ 
