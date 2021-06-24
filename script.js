daf30abea781ac9fcc1e92e1ef3b5f01a79e4699
stefandrei <stefandrei89@outlook.com>
2021-06-24 14:51:54 +0200

    Add/Ex-Basic2

--- script.js
+++ script.js
@@ -16,41 +16,63 @@
 //Age calculator// ex 2 and 3
  
 
-function ageCalculator(birth_year, current_year){
+// function ageCalculator(birth_year, current_year){
     
-    var myAge = current_year - birth_year;
-    var yourAge = myAge - 1;
-    return `You are either ${myAge} or ${yourAge}`;
+//     var myAge = current_year - birth_year;
+//     var yourAge = myAge - 1;
+//     return `You are either ${myAge} or ${yourAge}`;
 
-}
+// }
 
-document.write(ageCalculator(1989, 2021));
+// document.write(ageCalculator(1989, 2021));
 
 
-function ageCalculator2 (birthYear) {
+// function ageCalculator2 (birthYear) {
 
-    var currYear = new Date().getFullYear();
-    var myAge = currYear - birthYear;
-    var yourAge = myAge - 1;
-    return `You are either ${myAge} or ${yourAge}`;
+//     var currYear = new Date().getFullYear();
+//     var myAge = currYear - birthYear;
+//     var yourAge = myAge - 1;
+//     return `You are either ${myAge} or ${yourAge}`;
 
-}
-document.write("<br>")
-document.write(ageCalculator2(1989));
+// }
+// document.write("<br>")
+// document.write(ageCalculator2(1989));
 
-// document.write(new Date().getFullYear());
+// // document.write(new Date().getFullYear());
 
-// document.write(getFullYear());
+// // document.write(getFullYear());
 
-function converter (degrees) {
+// function converter (degrees) {
 
-    return degrees * (Math.PI / 180);
-}
+//     return degrees * (Math.PI / 180);
+// }
 
-document.write("<br>");
-document.write(converter(90));
 // document.write("<br>");
-// document.write(Math.PI);
+// document.write(converter(90));
+// // document.write("<br>");
+// // document.write(Math.PI);
 
 
 
+
+//Ex5//
+// function Box (w, h, d){
+//     var myArea = w*h;
+//     var myVolume = w*h*d;
+//     document.write(`The area of the box is: ${myArea} <br> The volume of the box is: ${myVolume}`);
+//     return
+    
+// }
+// Box(12,16,20);
+
+
+//ExBasic//
+
+// function Grade (m, p, e){
+//     var sum = m+p+e;
+//     var Av = sum / 3;
+//     document.write(`Sum${sum} <br> Av:${Av}`);
+//     return
+
+// }
+// Grade(3,4,5);
